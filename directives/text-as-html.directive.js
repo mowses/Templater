2da40d03a326bb7d272d32349e6051ff9e776b10
Moisés <moisesmosele@hotmail.com>
2016-09-01 01:49:52 -0300

    criado directive text-as-html. porem, detectei um problema nos regex do Templater. se rodar o index neste commit vera o problema

---
+++ directives/text-as-html.directive.js
@@ -0,0 +1,33 @@
+;(function($, Templater) {
+	"use strict";
+
+	Templater.Directive.extend({
+		name: 'text-as-html',
+		myView: null,
+
+		onInit: function() {
+			
+		},
+
+		onRender: function() {
+			this.myView && this.myView.destroy();  // remove previous view before get text
+			
+			var $element = this.view.$element;
+			var text = $element.text();
+			
+			this.myView = Templater.createFromHtml(text).generateView();
+			this.myView.setParentView(this.view);
+			this.myView.render($element, 'replaceWith');
+		}
+
+		/*htmlUnescape(str){
+			return str
+				.replace(/&quot;/g, '"')
+				.replace(/&#39;/g, "'")
+				.replace(/&lt;/g, '<')
+				.replace(/&gt;/g, '>')
+				.replace(/&amp;/g, '&');
+		}*/
+	});
+
+})(jQuery, Templater);
